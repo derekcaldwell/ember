14f02add3946e71841db9cdcbafa6763c591812a
Godfrey Chan <godfreykfc@gmail.com>
2016-07-29 14:05:55 -0700

    WIP

--- packages/ember-views/lib/mixins/view_child_views_support.js
+++ packages/ember-views/lib/mixins/view_child_views_support.js
@@ -12,6 +12,72 @@ import { getOwner, setOwner, OWNER } from 'container/owner';
 
 var EMPTY_ARRAY = [];
 
+export function removeChildView(parentView, childView) {
+  // If we're destroying, the entire subtree will be
+  // freed, and the DOM will be handled separately,
+  // so no need to mess with childViews.
+  if (parentView.isDestroying) { return; }
+
+  // update parent node
+  parentView.unlinkChild(childView);
+
+  // remove view from childViews array.
+  var childViews = get(parentView, 'childViews');
+
+  var index = childViews.indexOf(childView);
+  if (index !== -1) { childViews.splice(index, 1); }
+
+  return parentView;
+}
+
+export function createChildView(parentView, maybeViewClass, _attrs) {
+  if (!maybeViewClass) {
+    throw new TypeError('createChildView\'s first argument must exist');
+  }
+
+  let owner = getOwner(parentView);
+
+  if (maybeViewClass.isView && maybeViewClass.parentView === parentView && getOwner(maybeViewClass) === owner) {
+    return maybeViewClass;
+  }
+
+  var attrs = _attrs || {};
+  var view;
+
+  attrs.parentView = parentView;
+  attrs.renderer = parentView.renderer;
+  attrs._viewRegistry = parentView._viewRegistry;
+
+  if (maybeViewClass.isViewFactory) {
+    setOwner(attrs, owner);
+
+    view = maybeViewClass.create(attrs);
+
+    if (view.viewName) {
+      set(parentView, view.viewName, view);
+    }
+  } else if ('string' === typeof maybeViewClass) {
+    var fullName = 'view:' + maybeViewClass;
+    var ViewKlass = owner._lookupFactory(fullName);
+
+    assert('Could not find view: \'' + fullName + '\'', !!ViewKlass);
+
+    view = ViewKlass.create(attrs);
+  } else {
+    view = maybeViewClass;
+    assert('You must pass instance or subclass of View', view.isView);
+
+    setOwner(attrs, owner);
+    setProperties(view, attrs);
+  }
+
+  parentView.linkChild(view);
+
+  return view;
+}
+
+let _createChildView = createChildView;
+
 export default Mixin.create({
   /**
     Array of child views. You should never edit this array directly.
@@ -51,21 +117,7 @@ export default Mixin.create({
     @private
   */
   removeChild(view) {
-    // If we're destroying, the entire subtree will be
-    // freed, and the DOM will be handled separately,
-    // so no need to mess with childViews.
-    if (this.isDestroying) { return; }
-
-    // update parent node
-    this.unlinkChild(view);
-
-    // remove view from childViews array.
-    var childViews = get(this, 'childViews');
-
-    var index = childViews.indexOf(view);
-    if (index !== -1) { childViews.splice(index, 1); }
-
-    return this;
+    return removeChildView(this, view);
   },
 
   /**
@@ -82,49 +134,7 @@ export default Mixin.create({
     @private
   */
   createChildView(maybeViewClass, _attrs) {
-    if (!maybeViewClass) {
-      throw new TypeError('createChildViews first argument must exist');
-    }
-
-    let owner = getOwner(this);
-
-    if (maybeViewClass.isView && maybeViewClass.parentView === this && getOwner(maybeViewClass) === owner) {
-      return maybeViewClass;
-    }
-
-    var attrs = _attrs || {};
-    var view;
-
-    attrs.parentView = this;
-    attrs.renderer = this.renderer;
-    attrs._viewRegistry = this._viewRegistry;
-
-    if (maybeViewClass.isViewFactory) {
-      setOwner(attrs, owner);
-
-      view = maybeViewClass.create(attrs);
-
-      if (view.viewName) {
-        set(this, view.viewName, view);
-      }
-    } else if ('string' === typeof maybeViewClass) {
-      var fullName = 'view:' + maybeViewClass;
-      var ViewKlass = owner._lookupFactory(fullName);
-
-      assert('Could not find view: \'' + fullName + '\'', !!ViewKlass);
-
-      view = ViewKlass.create(attrs);
-    } else {
-      view = maybeViewClass;
-      assert('You must pass instance or subclass of View', view.isView);
-
-      setOwner(attrs, owner);
-      setProperties(view, attrs);
-    }
-
-    this.linkChild(view);
-
-    return view;
+    return _createChildView(this, maybeViewClass, _attrs);
   },
 
   linkChild(instance) {
