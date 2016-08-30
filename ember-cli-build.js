c9da336d4e0d9d3b4cbf723bc1d0fb85380d6264
Kris Selden <kris.selden@gmail.com>
2016-08-30 16:05:03 -0700

    cleanup

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,172 +2,103 @@
 const Funnel = require('broccoli-funnel');
 const MergeTrees = require('broccoli-merge-trees');
 const StringReplace = require('broccoli-string-replace');
-const Babel = require('broccoli-babel-transpiler');
-const getGitInfo = require('git-repo-info');
-const path = require('path');
+const Babel = require('broccoli-babel-transpiler');const path = require('path');
 const fs = require('fs');
 const Rollup = require('broccoli-rollup');
 const WriteFile = require('broccoli-file-creator');
-const moduleResolver = require('amd-name-resolver').resolveModules({
-  throwOnRootAccess: true
-});
 
-const EMBER_VERSION = getVersion();
+const FEATURES = require('./broccoli/features');
+const EMBER_VERSION = require('./broccoli/version');
 const VERSION_PLACEHOLDER = /VERSION_STRING_PLACEHOLDER/g;
-const RAW_FEATURES = fs.readFileSync('./features.json', {
-  encoding: 'utf8'
-});
-const PROD_FEATURES = getFeatures('production');
-const DEBUG_FEATURES = getFeatures('development');
 const REMOVE_LIB = /^([^\/]+\/)lib\//;
 
 module.exports = function () {
-  let emberAMDLib = es6ToNamedAMD(emberES6LibPackages());
+  let esTree = new Funnel(new MergeTrees([
+    rsvpES(),
+    routeRecognizerES(),
+    backburnerES(),
+    emberES(),
+    emberFeaturesES(),
+    emberVersionES()
+  ], {
+    annotation: 'es tree'
+  }), {
+    include: ['**/*.js'],
+    destDir: 'es',
+    annotation: 'es tree'
+  });
+
   return new MergeTrees([
-    emberAMDLib,
-    emberVersion(),
-    emberFeatures(),
-    rsvpAMD(),
-    routeRecognizer(),
-    backburner()
+    esTree,
+    qunit(),
+    jquery(),
+    testIndexHTML(),
+    packageManagerJSONs()
   ], {
     annotation: 'dist'
   });
 };
 
-function emberVersion() {
-  var content = 'export default ' + JSON.stringify(EMBER_VERSION) + ';\n';
-  var tree = new WriteFile('ember/version.js', content, {
-    annotation: 'ember/version'
-  });
-  return es6ToNamedAMD(tree);
-}
-
-function emberFeatures() {
-  var content = 'export default ' + JSON.stringify(DEBUG_FEATURES) + ';\n';
-  var tree = new WriteFile('ember/features.js', content, {
-    annotation: 'ember/features (DEBUG)'
-  });
-  return es6ToNamedAMD(tree);
-}
-
-function routeRecognizer() {
-  let dist = path.dirname(require.resolve('route-recognizer'));
-  let es6 = path.join(dist, 'es6');
-  return new Rollup(es6, {
-    rollup: {
-      entry: 'route-recognizer.js',
-      targets: [{
-        dest: 'route-recognizer.js',
-        format: 'amd',
-        moduleId: 'route-recognizer',
-        exports: 'named'
-      }]
-    }
-  });
-}
-
-function backburner() {
-  let dist = path.dirname(require.resolve('backburner.js'));
-  let es6 = path.join(dist, 'es6');
-  return new Rollup(es6, {
-    rollup: {
-      entry: 'backburner.js',
-      targets: [{
-        dest: 'backburner.js',
-        format: 'amd',
-        moduleId: 'backburner',
-        exports: 'named'
-      }]
-    }
-  });
-}
-
-function rsvpAMD() {
-  let version = require('./bower_components/rsvp/package.json').version;
-  let banner = fs.readFileSync('bower_components/rsvp/config/versionTemplate.txt', 'utf8');
-  let rsvp = new Rollup('bower_components/rsvp/lib', {
-    rollup: {
-      entry: 'rsvp.js',
-      targets: [{
-        banner: banner.replace('VERSION_PLACEHOLDER_STRING', version),
-        dest: 'rsvp.js',
-        format: 'amd',
-        moduleId: 'rsvp',
-        exports: 'named'
-      }]
-    },
-    annotation: 'rsvp.js'
-  });
-  return rsvp;
-}
-
-function emberES6LibPackages() {
-  let removeLib = /^([^\/]+\/)lib\//;
+function emberES() {
   return new Funnel('packages', {
     include: ['*/lib/**/*.js'],
-    exclude: ['loader/**'],
+    exclude: ['loader/**', 'external-helpers/**', 'internal-test-helpers/**'],
     getDestinationPath(relativePath) {
-      // TODO const regex
-      return relativePath.replace(removeLib, "$1");
+      return relativePath.replace(REMOVE_LIB, "$1");
     },
     annotation: 'packages ES6'
   });
 }
 
-function es6ToNamedAMD(tree) {
-  var options = {
-    passPerPreset: true,
-    moduleIds: true,
-
-    resolveModuleSource: moduleResolver,
-
-    plugins: [
-      function (opts) {
-        let t = opts.types;
-        return {
-          pre(file) {
-            file.set("helperGenerator", function (name) {
-              if (name === 'interopRequireDefault') {
-                // goes into a call expression
-                return t.functionExpression(null, [
-                  t.identifier('m')
-                ], t.blockStatement([
-                  t.returnStatement(t.identifier('m'))
-                ]));
-              }
-              return t.memberExpression(
-                t.identifier('EmBabel'),
-                t.identifier(name)
-              );
-            });
-          }
-        };
-      },
-      ['transform-es2015-template-literals', {loose: true}],
-      ['transform-es2015-arrow-functions'],
-      ['transform-es2015-destructuring', {loose: true}],
-      ['transform-es2015-spread', {loose: true}],
-      ['transform-es2015-parameters'],
-      ['transform-es2015-computed-properties', {loose: true}],
-      ['transform-es2015-shorthand-properties'],
-      ['transform-es2015-block-scoping'],
-      ['check-es2015-constants'],
-      ['transform-es2015-classes', {loose: true}],
-      ['transform-proto-to-assign'],
-      ['transform-es2015-modules-amd']
-    ]
-  };
-  let babel = new Babel(tree, options);
-  let origKey = babel.cacheKey;
-  babel.cacheKey = function () {
-    let key = origKey.apply(this, arguments);
-    return key + "sdsdfsf";
-  }
-  babel._annotation = 'packages named AMD';
-  return babel;
+function emberVersionES() {
+  let content = 'export default ' + JSON.stringify(EMBER_VERSION) + ';\n';
+  return new WriteFile('ember/version.js', content, {
+    annotation: 'ember/version'
+  });
 }
 
+function emberFeaturesES() {
+  let content = 'export default ' + JSON.stringify(FEATURES.DEBUG) + ';\n';
+  return new WriteFile('ember/features.js', content, {
+    annotation: 'ember/features (DEBUG)'
+  });
+}
+
+function backburnerES() {
+  let dist = path.dirname(require.resolve('backburner.js'));
+  dist = path.join(dist, 'es6');
+  return new Funnel(dist, {
+    files: ['backburner.js']
+  });
+}
+
+function rsvpES() {
+  let version = require('./bower_components/rsvp/package').version;
+  let banner = fs.readFileSync(
+    path.resolve(__dirname, 'bower_components/rsvp/config/versionTemplate.txt'),
+    'utf8');
+  return new Rollup('bower_components/rsvp/lib', {
+    rollup: {
+      entry: 'rsvp.js',
+      targets: [{
+        banner: banner.replace('VERSION_PLACEHOLDER_STRING', version),
+        dest: 'rsvp.js',
+        format: 'es'
+      }]
+    },
+    annotation: 'rsvp.js'
+  });
+}
+
+function routeRecognizerES() {
+  let dist = path.dirname(require.resolve('route-recognizer'));
+  let es6 = path.join(dist, 'es6');
+  return new Funnel(es6, {
+    files: ['route-recognizer.js']
+  });
+}
+
+// non bundled vendor
 function jquery() {
   let jquery = require.resolve('jquery');
   return new Funnel(path.dirname(jquery), {
@@ -177,6 +108,7 @@ function jquery() {
   });
 }
 
+// TEST files
 function qunit() {
   var qunitjs = require.resolve('qunitjs');
   return new Funnel(path.dirname(qunitjs), {
@@ -186,7 +118,7 @@ function qunit() {
   });
 }
 
-function testIndex() {
+function testIndexHTML() {
   let index = new Funnel('tests', {
     files: ['index.html'],
     destDir: 'tests',
@@ -196,17 +128,17 @@ function testIndex() {
     files: ['tests/index.html'],
     patterns: [{
       match: /\{\{DEV_FEATURES\}\}/g,
-      replacement: JSON.stringify(DEBUG_FEATURES)
+      replacement: JSON.stringify(FEATURES.DEBUG)
     }, {
       match: /\{\{PROD_FEATURES\}\}/g,
-      replacement: JSON.stringify(PROD_FEATURES)
+      replacement: JSON.stringify(FEATURES.RELEASE)
     }],
   });
   index._annotation = 'tests/index.html FEATURES';
   return index;
 }
 
-function packageManagerJsonFiles() {
+function packageManagerJSONs() {
   var packageJsons = new Funnel('config/package_manager_files', {
     include: ['*.json'],
     destDir: '/',
@@ -223,53 +155,55 @@ function packageManagerJsonFiles() {
   return packageJsons;
 }
 
-function getVersion() {
-  var projectPath = process.cwd();
-  var info = getGitInfo(projectPath);
-  if (info.tag) {
-    return info.tag.replace(/^v/, '');
-  }
+// function es6ToNamedAMD(tree) {
+//   var options = {
+//     passPerPreset: true,
+//     moduleIds: true,
 
-  var packageVersion  = require(path.join(projectPath, 'package.json')).version;
-  var sha = info.sha || '';
-  var prefix = packageVersion + '-' + (process.env.BUILD_TYPE || info.branch);
+//     resolveModuleSource: moduleResolver,
 
-  return prefix + '+' + sha.slice(0, 8);
-}
-
-function getFeatures(environment) {
-  var features = JSON.parse(RAW_FEATURES).features;
-  var featureName;
-
-  if (process.env.BUILD_TYPE === 'alpha') {
-    for (featureName in features) {
-      if (features[featureName] === null) {
-        features[featureName] = false;
-      }
-    }
-  }
-
-  if (process.env.OVERRIDE_FEATURES) {
-    var forcedFeatures = process.env.OVERRIDE_FEATURES.split(',');
-    for (var i = 0; i < forcedFeatures.length; i++) {
-      featureName = forcedFeatures[i];
-
-      features[featureName] = true;
-    }
-  }
-
-  features['ember-glimmer-allow-backtracking-rerender'] = false;
-
-  if (process.env.ALLOW_BACKTRACKING) {
-    features['ember-glimmer-allow-backtracking-rerender'] = true;
-    features['ember-glimmer-detect-backtracking-rerender'] = false;
-  }
-
-  var isDevelopment = (environment === 'development');
-  var isProduction = (environment === 'production');
-
-  features['mandatory-setter'] = isDevelopment;
-  features['ember-glimmer-detect-backtracking-rerender'] = isDevelopment;
-
-  return features;
-}
+//     plugins: [
+//       function (opts) {
+//         let t = opts.types;
+//         return {
+//           pre(file) {
+//             file.set("helperGenerator", function (name) {
+//               if (name === 'interopRequireDefault') {
+//                 // goes into a call expression
+//                 return t.functionExpression(null, [
+//                   t.identifier('m')
+//                 ], t.blockStatement([
+//                   t.returnStatement(t.identifier('m'))
+//                 ]));
+//               }
+//               return t.memberExpression(
+//                 t.identifier('EmBabel'),
+//                 t.identifier(name)
+//               );
+//             });
+//           }
+//         };
+//       },
+//       ['transform-es2015-template-literals', {loose: true}],
+//       ['transform-es2015-arrow-functions'],
+//       ['transform-es2015-destructuring', {loose: true}],
+//       ['transform-es2015-spread', {loose: true}],
+//       ['transform-es2015-parameters'],
+//       ['transform-es2015-computed-properties', {loose: true}],
+//       ['transform-es2015-shorthand-properties'],
+//       ['transform-es2015-block-scoping'],
+//       ['check-es2015-constants'],
+//       ['transform-es2015-classes', {loose: true}],
+//       ['transform-proto-to-assign'],
+//       ['transform-es2015-modules-amd']
+//     ]
+//   };
+//   let babel = new Babel(tree, options);
+//   let origKey = babel.cacheKey;
+//   babel.cacheKey = function () {
+//     let key = origKey.apply(this, arguments);
+//     return key + "sdsdfsf";
+//   }
+//   babel._annotation = 'packages named AMD';
+//   return babel;
+// }
